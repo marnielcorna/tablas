d819d9d267fac999d9cf00d4460bcb6ef90810e5
marnielcorna <83858563+marnielcorna@users.noreply.github.com>
2021-05-29 07:20:25 +0200

    proyecto recuperado filtros

--- scripts/matchescript.js
+++ scripts/matchescript.js
@@ -1,8 +1,33 @@
+let valueEquipos = document.getElementById("tagEquipoId");
+let radio_Ganados = document.getElementById("radio_Ganados");
+let radio_Perdidos = document.getElementById("radio_Perdidos");
+let radio_Empatados = document.getElementById("radio_Empatados");
+let radio_Todos = document.getElementById("radio_Todos");
+let infoPartidos = [];
+
 let partidos2020 = matches[0].matches;
-
 let tablebody = document.getElementById("table-body");
-// console.log(partidos2020[0].awayTeam.name);
 
+
+// let pintarEquiposObtenidos = (arrayEquiposObtenidos) =>{
+//     for(let i=0; i<arrayEquiposObtenidos.length; i++){
+//         pintarEquipos(arrayEquiposObtenidos[i]);
+//     }
+// };
+
+// let pintarEquipos = (obj)=>{
+//     let tr = document.createElement("tr");
+//     tr.innerHTML =`<td>${equipoVisitante}</td><td><img class="logoescudo" src="https://crests.football-data.org/${partidos2020[i].awayTeam.id}.svg"><td style="text-align: center">${marcador}</td><td><img class="logoescudo" src="https://crests.football-data.org/${partidos2020[i].homeTeam.id}.svg"><td>${equipoLocal}</td>`;
+//     tablebody.appendChild(tr);
+
+// }
+
+
+// pintarEquiposObtenidos(partidos2020);
+
+// <----------------------------------------------------------------->
+
+console.log(infoPartidos);
 
 for (let i= 0; i<partidos2020.length; i++){
 
@@ -12,6 +37,15 @@ for (let i= 0; i<partidos2020.length; i++){
     let awayT = partidos2020[i].score.fullTime.awayTeam;
     let marcador = partidos2020[i].score.fullTime; 
     let pendientes = partidos2020[i].score.winner;
+    let infoPartido ={
+        equipoVisitante, 
+        equipoLocal, 
+        homeT, 
+        awayT, 
+        marcador, 
+        pendientes,};
+    infoPartidos.push(infoPartido);
+
     marcador = `${homeT} - ${awayT}`;
 
     if(pendientes == null){
